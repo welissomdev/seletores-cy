3fc1e0cb120b15765292967f5db268268b1f7cd7
welissom <welyssom155@gmail.com>
2024-03-05 19:58:19 -0300

    Automatizações de cenários

--- cypress/e2e/seletores.cy.js
+++ cypress/e2e/seletores.cy.js
@@ -1,4 +1,4 @@
-///<reference types = "cypress"/>
+//<reference types = "cypress"/>
 
 describe('Seletores avançados com cypress', () => {
 
@@ -7,51 +7,53 @@ describe('Seletores avançados com cypress', () => {
   });
 
   it('Seleciona elementos que contêm um Texto específico', () => {
-    //TODO: 
+    cy.contains('Item 1').should('have.attr','class','filho-1')
   });
     
   it('Seleciona o elemento com a classe pai', () => {
-    //TODO: 
+    cy.get('.pai').should('exist')
   })
 
   it('Seleciona o elemento com o id Filho', () => {
-    //TODO: 
+    cy.get('#id-filho').should('exist')
    })
 
   it('Seleciona um elemento filho dentro do elemento com a classe pai', () => {
-    //TODO: 
+    cy.get('.pai').find('.filho-2').should('contain','Item 2')
   });
 
   it('Seleciona o segundo elemento <span> com a classe irmao', () => {
-    //TODO: 
+    cy.get('.irmao + .irmao').should('contain','Irmão 2')
   });
 
   it('Seleciona o próximo elemento irmão', () => {
-    //TODO: 
+    cy.get('#irmao-1').next().should('contain','Irmão 2')
   });
 
   it('Seleciona o elemento irmão anterior', () => {
-    //TODO: 
+    cy.get('#irmao-2').prev().should('contain','Irmão 1')
   });
 
   it('Seleciona o irmão da div anterior', () => {
-    //TODO: 
+    cy.get('[name="nome-do-atributo"]').prev().should('contain','Item 1')
   });
 
   it('Seleciona o terceiro elemento <li> encontrado', () => {
-    //TODO: 
+    cy.get('li').eq(2).should('have.text','Item 3')
   });
 
   it('Seleciona o elemento com o atributo data-test', () => {
-    //TODO: 
+    cy.get('[data-test="div-pai"]').should('exist')
   });
 
   it('Seleciona o elemento com a classe pai do elemento com a classe filho', () => {
-    //TODO: 
+    cy.get('.filho-4').parent('[data-test="div-pai"]').should('have.attr', 'class', 'pai')
   });
 
   it('Seleciona o elemento com um valor em um select', () => {
-    //TODO: 
-  });
+    cy.get('[name="opcao"]').select('Médio')
+    cy.get('#id-enviar').click()
+    cy.get('#mensagemFeedback').should('have.text','Obrigado por compartilhar conosco!')
+});
 
 })
